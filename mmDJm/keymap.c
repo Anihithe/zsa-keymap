9a0ec618587c07cef47dd5653451b79baaa6bbd8
Anihithe <anihithe@gmail.com>
2025-03-23 21:26:42 +0100

    fix(code): try new send string

--- mmDJm/keymap.c
+++ mmDJm/keymap.c
@@ -13,17 +13,17 @@ enum custom_keycodes {
   ST_MACRO_1,
 
   // CUSTOM KC
-  ST_MACRO_AGRAV,
-  ST_MACRO_EGRAV
+  AGRAV,
+  EGRAV
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     US_HASH,        ST_MACRO_0,     ST_MACRO_1,     CT_DQOT,        KC_MINUS,       US_PLUS,        KC_NO,                                          KC_NO,          US_ASTR,        KC_SLASH,       KC_EQUAL,       US_LPRN,        US_RPRN,        US_DLR,         
-    KC_AT,          ST_MACRO_AGRAV, KC_J,           KC_O,           US_EACU,        KC_B,           KC_NO,                                          KC_NO,          KC_F,           KC_D,           KC_L,           KC_QUOTE,       KC_Q,           KC_X,           
+    KC_AT,          AGRAV, KC_J,           KC_O,           US_EACU,        KC_B,           KC_NO,                                          KC_NO,          KC_F,           KC_D,           KC_L,           KC_QUOTE,       KC_Q,           KC_X,           
     KC_LEFT_SHIFT, KC_A,           KC_I,           KC_E,           KC_U,           KC_COMMA,       KC_NO,                                                                          KC_NO,          KC_P,           KC_T,           KC_S,           KC_R,           KC_N,           US_CIRC,        
-    KC_ESCAPE,      KC_K,           KC_Y,           ST_MACRO_EGRAV, KC_DOT,         KC_W,                                           KC_G,           KC_C,           KC_M,           KC_H,           KC_V,           KC_W,           
+    KC_ESCAPE,      KC_K,           KC_Y,           EGRAV, KC_DOT,         KC_W,                                           KC_G,           KC_C,           KC_M,           KC_H,           KC_V,           KC_W,           
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                                                                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
     KC_SPACE,       KC_TAB,         KC_NO,                          KC_NO,          KC_BSPC,        KC_ENTER
   ),
@@ -90,23 +90,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_8) SS_TAP(X_KP_7) ));
     }
     break;
-    case ST_MACRO_AGRAV:
+    case AGRAV:
       if(record->event.pressed) {
         if (mod_state & MOD_MASK_SHIFT) {
           del_mods(MOD_MASK_SHIFT);
-          SEND_STRING(SS_TAP(X_GRV) SS_LSFT(SS_TAP(X_A)));
+          SEND_STRING(SS_TAP(X_GRV));
           set_mods(mod_state);
+          SEND_STRING(SS_TAP(X_A));
         } else {
           SEND_STRING(SS_TAP(X_GRV) SS_TAP(X_A));
         }
       }
       break;
-    case ST_MACRO_EGRAV:
+    case EGRAV:
       if(record->event.pressed) {
         if (mod_state & MOD_MASK_SHIFT) {
           del_mods(MOD_MASK_SHIFT);
-          SEND_STRING(SS_TAP(X_GRV) SS_LSFT(SS_TAP(X_E)));
+          SEND_STRING(SS_TAP(X_GRV));
           set_mods(mod_state);
+          SEND_STRING(SS_TAP(X_E));
         } else {
           SEND_STRING(SS_TAP(X_GRV) SS_TAP(X_E));
         }
