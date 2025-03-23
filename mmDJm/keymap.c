67a58a518869a7217fc92f7f52529cd4ef714728
Anihithe <anihithe@gmail.com>
2025-03-23 16:49:03 +0100

    add(macro): add à and è

--- mmDJm/keymap.c
+++ mmDJm/keymap.c
@@ -5,18 +5,25 @@
 #define ML_SAFE_RANGE SAFE_RANGE
 
 enum custom_keycodes {
+  // RGB
   RGB_SLD = ML_SAFE_RANGE,
+
+  // MACRO
   ST_MACRO_0,
   ST_MACRO_1,
+
+  // CUSTOM KC
+  ST_MACRO_AGRAV,
+  ST_MACRO_ECIRC
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    US_HASH,        ST_MACRO_0,     ST_MACRO_1,     CT_DQOT,          KC_MINUS,       US_PLUS,        KC_NO,                                          KC_NO,          US_ASTR,        KC_SLASH,       KC_EQUAL,       US_LPRN,        US_RPRN,        US_DLR,         
-    KC_AT,          KC_TRANSPARENT, KC_J,           KC_O,           US_EACU,        KC_B,           KC_NO,                                          KC_NO,          KC_F,           KC_D,           KC_L,           KC_QUOTE,       KC_Q,           KC_X,           
+    US_HASH,        ST_MACRO_0,     ST_MACRO_1,     CT_DQOT,        KC_MINUS,       US_PLUS,        KC_NO,                                          KC_NO,          US_ASTR,        KC_SLASH,       KC_EQUAL,       US_LPRN,        US_RPRN,        US_DLR,         
+    KC_AT,          ST_MACRO_AGRAV, KC_J,           KC_O,           US_EACU,        KC_B,           KC_NO,                                          KC_NO,          KC_F,           KC_D,           KC_L,           KC_QUOTE,       KC_Q,           KC_X,           
     KC_TRANSPARENT, KC_A,           KC_I,           KC_E,           KC_U,           KC_COMMA,       KC_NO,                                                                          KC_NO,          KC_P,           KC_T,           KC_S,           KC_R,           KC_N,           US_CIRC,        
-    KC_ESCAPE,      KC_K,           KC_Y,           KC_TRANSPARENT, KC_DOT,         KC_W,                                           KC_G,           KC_C,           KC_M,           KC_H,           KC_V,           KC_W,           
+    KC_ESCAPE,      KC_K,           KC_Y,           ST_MACRO_ECIRC, KC_DOT,         KC_W,                                           KC_G,           KC_C,           KC_M,           KC_H,           KC_V,           KC_W,           
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                                                                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
     KC_SPACE,       KC_TAB,         KC_NO,                          KC_NO,          KC_BSPC,        KC_ENTER
   ),
@@ -69,7 +76,10 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  mod_state = get_mods();
   switch (keycode) {
+
+    //MACRO
     case ST_MACRO_0:
     if (record->event.pressed) {
       SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_7) SS_TAP(X_KP_1) ));
@@ -80,6 +90,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_8) SS_TAP(X_KP_7) ));
     }
     break;
+    case ST_MACRO_AGRAV:
+      if(record->event.pressed) {
+        if (mod_state & MOD_MASK_SHIFT) {
+          SEND_STRING(SS_TAP(X_GRV) SS_LSFT(SS_TAP(X_A)))
+        } else {
+          SEND_STRING(SS_TAP(X_GRV) SS_TAP(X_A));
+        }
+      }
+      break;
+    case ST_MACRO_ECIRC:
+      if(record->event.pressed) {
+        if (mod_state & MOD_MASK_SHIFT) {
+          SEND_STRING(SS_TAP(X_QUOTE) SS_LSFT(SS_TAP(X_E)))
+        } else {
+          SEND_STRING(SS_TAP(X_QUOTE) SS_TAP(X_E))
+        }
+      }
 
     case RGB_SLD:
         if (rawhid_state.rgb_control) {
